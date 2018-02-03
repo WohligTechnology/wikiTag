fb4e91bf1021d14c3738131025d5f94ef4e31db9
sohan1 <shn619@gmail.com>
2018-02-03 13:18:49 +0530

    bulidd

---
+++ platforms/android/app/src/main/assets/www/js/app.js
@@ -0,0 +1,85 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','ngCordova'])
+
+.run(function($ionicPlatform) {
+  // var app = {
+  //   requiredFeatures: [ "2d_tracking", "geo" ],
+  //   isDeviceSupported: false,
+  //   startupConfiguration:
+  //   {
+  //       "camera_position": "back"
+  //   }
+  // }
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+
+    // CALL WIKITUDE FILE
+
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.scanner', {
+    url: '/scanner',
+    views: {
+      'tab-scanner': {
+        templateUrl: 'templates/tab-scanner.html',
+        controller: 'ScannerCtrl'
+      }
+    }
+  })
+
+  .state('tab.suggest', {
+      url: '/suggest',
+      views: {
+        'tab-suggest': {
+          templateUrl: 'templates/tab-suggest.html',
+          controller: 'SuggestionCtrl'
+        }
+      }
+    })
+    .state('tab.chat-detail', {
+      url: '/chats/:chatId',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/chat-detail.html',
+          controller: 'ChatDetailCtrl'
+        }
+      }
+    });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/suggest');
+
+});
